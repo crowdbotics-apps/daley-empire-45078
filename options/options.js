05c3638e453bd6bb6963d4eb6174c749139aa61f
Crowdbotics <team@crowdbotics.com>
2023-12-18 14:50:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "daley-empire-45078",
+  url: "https://daley-empire-45078.botics.co",
+  api: "https://daley-empire-45078.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
